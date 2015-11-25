27c1ad6112b71bb7de60486f5203cfc8673c16b5
jay shah <jay@bayesconsulting.io>
2015-11-25 00:03:52 -0600

    using apscheduler and redis to hit cap metro api

--- clock.py
+++ clock.py
@@ -1,15 +1,21 @@
 # misc
 import pdb, sys, datetime
 from api_fns.api import get_bus_data
+import logging
+
+logging.basicConfig()
 
 # scheduling
 from apscheduler.schedulers.blocking import BlockingScheduler
+from rq import Queue
+from worker import conn
 
 sched = BlockingScheduler()
+q = Queue(connection=conn)
 
-@sched.scheduled_job('interval',seconds=5)
+@sched.scheduled_job('interval',seconds=1)
 def hit_api():
-    vehicles = get_bus_data()
+    vehicles = q.enqueue(get_bus_data,)
     print datetime.datetime.now()
     sys.stdout.flush()
        
